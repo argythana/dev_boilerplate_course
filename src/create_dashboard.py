1f5d03b00925855debeb19a400f77057d86bb22b
Thanasis Argyriou <argythana@gmail.com>
2023-10-17 12:23:57 +0200

    Update dependencies and add dashboard functions.

---
+++ src/create_dashboard.py
@@ -0,0 +1,5 @@
+from dashboard_functions import plot_daily_dashboard, datafile
+
+if __name__ == "__main__":
+    # datafile = "./data/run4moreProd.activities.csv"
+    plot_daily_dashboard(datafile)
