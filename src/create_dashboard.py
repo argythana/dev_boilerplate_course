f2dd898caf5319823d9ffc40c7d08cace2b3fb56
tharg <argythana@gmail.com>
2023-10-18 08:54:34 +0200

    Refactor create_dashboard script to be update with changes in dashboard functions changes.

--- src/create_dashboard.py
+++ src/create_dashboard.py
@@ -1,5 +1,5 @@
-from dashboard_functions import plot_daily_dashboard, datafile
+from dashboard_functions import plot_daily_dashboard, activities_datafile
 
 if __name__ == "__main__":
     # datafile = "./data/run4moreProd.activities.csv"
-    plot_daily_dashboard(datafile)
+    plot_daily_dashboard(activities_datafile)
